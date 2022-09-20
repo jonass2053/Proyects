752aea7ed7b751c49c9a707b3b6d6d7ba98d7b24
jonass2053 <jonass2053@gmail.com>
2022-09-20 05:39:50 +0200

    20-9-2022 nueva version

---
+++ JavaScrips/cortes.js
@@ -0,0 +1,4 @@
+let pliego  =document.getElementById("fila");
+console.log(pliego);
+let row=0;
+let col=0;
--- JavaScrips/js.js
+++ JavaScrips/js.js
@@ -1,68 +1,145 @@
-class Datos
-{   
-    constructor()
-    {
-        this.anchom;
-        this.alltom;
-        this.anchop;
-        this.altop;
 
+
+
+
+
+
+let rows;
+let colums;
+let contador = 1;
+let valor1, valor2, valor3, valor4;
+
+
+const verificacion =()=>
+{
+    let pliego = document.getElementById("pliego");
+    let arrFilas = document.getElementsByClassName("filapadre");
+    let dimension = document.getElementById("dimension");
+    if (arrFilas.length > 0) {
+        console.log("entre");
+        arrFilas[0].parentNode.remove();
+        let pliego = document.createElement("div");
+        pliego.id = "pliego";
+        pliego.className = "pliego";
+        dimension.appendChild(pliego);
     }
+}
 
+
+const crearCortes = (val1, val2, ancho, alto) => {
+        verificacion();
+       let pliego = document.getElementById("pliego");
+
+        for (let index = 0; index < val2; index++) {
+            let fila = document.createElement("div");
+            fila.className = "row corte filapadre"
+        
+
+            for (let index = 0; index < val1; index++) {
+                let col = document.createElement("div");
+                col.className = "col corte colcorte "
+                col.innerHTML = `${contador}`;
+                contador++;
+                col.style.color = "white"
+                col.style.width=`${ancho}0%`;
+                col.style.height=`${alto}0%`;
+              
+                fila.appendChild(col);
+
+            }
+            pliego.appendChild(fila);
+
+
+        
+
+
+        }
+
+        // eliminar cortes 
+
+
+
+        // columnas
+
+
+
+
+
+        contador = 1;
+    
+
+
+    
 
 
 }
 
-const obj = new Datos();
-function Calcular(anchom, alltom, anchop, altop)
-{
+function Calcular(anchom, alltom, anchop, altop) {
     let temp;
-    let valor1=anchom / anchop;
-    let valor2= alltom/altop;
-    let resultado1 = parseInt(valor1) * parseInt(valor2);
-   
-    temp=anchom;
-    anchom=alltom;
-    alltom=temp;
-     valor1=anchom / anchop;
-     valor2= alltom/altop;
-    let resultado2=parseInt(valor1) * parseInt(valor2);
-  
+     valor1 = anchom / anchop;
+     valor2 = alltom / altop;
 
-    if(resultado1>resultado2)
-    {
+    let resultado1 = parseInt(valor1) * parseInt(valor2);
+
+    temp = anchom;
+    anchom = alltom;
+    alltom = temp;
+     valor3 = anchom / anchop;
+     valor4 = alltom / altop;
+    let resultado2 = parseInt(valor3) * parseInt(valor4);
+
+    if (resultado1 > resultado2) {
+
+        rows = parseInt(valor1);
+        colums = parseInt(valor2);
+
+        crearCortes(rows, colums, anchop, altop);
+
+
+
+        console.log(`numero1 ${rows}, ${colums}`);
+
         return resultado1;
 
+
+
+
     }
-    else
-    {
-       return resultado2;
+    else {
+        rows = parseInt(valor3);
+        colums = parseInt(valor4);
+        console.log(`numero1 ${rows}, ${colums}`);
+
+        crearCortes(rows, colums);
+
+
+        1
+        return resultado2;
+
     }
-    
+
+
 }
 
-function Ejecutar()
-{
-    let valor1=document.getElementById("textanchom").value;
-    let valor2=document.getElementById("textaltom").value;
-    let valor3=document.getElementById("textanchop").value;
-    let valor4=document.getElementById("textaltop").value;
-    if(valor1>0 && valor1!="" && valor2>0 && valor2!="" && valor3>0 && valor3!="" && valor4>0 && valor4!="")
-    {
-        let resultado=Calcular(valor1, valor2, valor3, valor4);
-    
+function Ejecutar() {
+    let valor1 = document.getElementById("textanchom").value;
+    let valor2 = document.getElementById("textaltom").value;
+    let valor3 = document.getElementById("textanchop").value;
+    let valor4 = document.getElementById("textaltop").value;
+    if (valor1 > 0 && valor1 != "" && valor2 > 0 && valor2 != "" && valor3 > 0 && valor3 != "" && valor4 > 0 && valor4 != "") {
+        let resultado = Calcular(valor1, valor2, valor3, valor4);
+
         const resultadopadre = document.getElementById("resultadopadre");
-        const resultadohijo= document.getElementById("resultado");
-        resultadohijo.style.color="red";
-        resultadohijo.style.fontSize="50px"
-        resultadohijo.innerHTML=resultado;
-        
+        const resultadohijo = document.getElementById("resultado");
+        resultadohijo.style.color = "green";
+        resultadohijo.style.fontSize = "50px"
+        resultadohijo.innerHTML = resultado;
+
     }
-    else
-    {
+    else {
         alert("Se ha producido un error por favor intentelo de nuevo...");
     }
-    
+
 
 
 
@@ -73,6 +150,9 @@ function Ejecutar()
 
 
 
+
+
+//let pliego  =document.getElementById("fila");
 
 
 
